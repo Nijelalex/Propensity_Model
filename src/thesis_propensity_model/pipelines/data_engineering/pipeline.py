5e6bd6ca2e5686c9fba3f8addd665fe942000718
Nijel Alex <nijel.p.alex@gmail.com>
2022-08-13 20:18:24 +0400

    Commit pipeline change

--- src/thesis_propensity_model/pipelines/data_engineering/pipeline.py
+++ src/thesis_propensity_model/pipelines/data_engineering/pipeline.py
@@ -1,6 +1,4 @@
-"""Example code for the nodes in the example pipeline. This code is meant
-just for illustrating basic Kedro features.
-Delete this when you start working on your own Kedro project.
+"""Data Preprocessing Node
 """
 
 from kedro.pipeline import node, pipeline
