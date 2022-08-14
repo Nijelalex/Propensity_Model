464d04bdcbb212ec12fb8c068a011e5c41cf6d16
Nijel Alex <nijel.p.alex@gmail.com>
2022-08-14 11:17:27 +0400

    Create de catalog and initiated ds

---
+++ src/thesis_propensity_model/pipelines/data_science/__init__.py
@@ -0,0 +1,1 @@
+from .pipeline import create_ds_pipeline
---
+++ src/thesis_propensity_model/pipelines/data_science/pipeline.py
@@ -0,0 +1,34 @@
+"""Data Science Pipeline
+"""
+
+from kedro.pipeline import node, pipeline
+from .nodes import outlier_removal,data_imputation,feature_selection_dedupe
+
+
+def create_ds_pipeline(**kwargs):
+    return pipeline(
+        [
+            node(
+                func=outlier_removal,
+                inputs=["bank_raw","params:de_params"],
+                outputs="outlier_removed_table",
+                name="Outlier_Removal",
+                tags="de",
+            ),
+            node(
+                func=data_imputation,
+                inputs=["outlier_removed_table","params:de_params"],
+                outputs="imputed_table",
+                name="Data_Imputation",
+                tags="de",
+            ),
+            node(
+                func=feature_selection_dedupe,
+                inputs=["imputed_table","params:de_params"],
+                outputs="fs_table",
+                name="Feature_Selection",
+                tags="de",
+            ),
+        ],
+        
+    )
